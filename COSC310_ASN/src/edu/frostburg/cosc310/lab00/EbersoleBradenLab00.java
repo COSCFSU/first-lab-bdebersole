4ca057bcae37e63893ee202a9efd17ef091531dd
Braden <Braden@Sword.frostburg.edu>
2018-09-07 09:54:03 -0400

    This is the start of Lab00

---
+++ COSC310_ASN/src/edu/frostburg/cosc310/lab00/EbersoleBradenLab00.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.frostburg.cosc310.lab00;
+
+/**
+ *
+ * @author Braden
+ */
+public class EbersoleBradenLab00 implements Lab00{
+    
+    @Override
+    public 
+}
