aa85c5b97dfb4e0cd724bf8cac47b092b959c1f4
Braden <Braden@Sword.frostburg.edu>
2018-09-07 16:22:24 -0400

    The finished product with added methods

--- COSC310_ASN/src/edu/frostburg/cosc310/lab00/EbersoleBradenLab00.java
+++ COSC310_ASN/src/edu/frostburg/cosc310/lab00/EbersoleBradenLab00.java
@@ -1,16 +1,61 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Braden Ebersole
+ * sept 7, 2018
+ * 
  */
 package edu.frostburg.cosc310.lab00;
+import java.util.ArrayList; 
+import java.util.LinkedList; 
 
 /**
- *
- * @author Braden
+ * @version 2018.7.9
+ * @author Braden Ebersole
  */
 public class EbersoleBradenLab00 implements Lab00{
     
     @Override
-    public 
+    public long problem0(int size) {
+        System.out.println("This is just an example, but please follow this template.");
+        String s = "";
+        long start = System.currentTimeMillis();
+
+        for (int i=0; i<size; i++) {
+            s = s + i;
+        }
+        
+        long end = System.currentTimeMillis();
+        return end - start;
+    }
+    
+    @Override
+    public long problem1(int size) {
+        StringBuilder sb = new StringBuilder();
+        long start = System.currentTimeMillis();
+        for (int i=0; i<size; i++) {
+            sb.append(i);
+        }
+        long end = System.currentTimeMillis();
+        return end - start;
+    }
+    
+    @Override
+    public long problem2() {
+        ArrayList<String> arrList = new ArrayList<>();
+        long start = System.currentTimeMillis(); 
+        for(int i = 0; i<1234567; i++){
+            arrList.add(Integer.toString(i));
+        }
+        long end = System.currentTimeMillis();  
+        return end - start;        
+    }
+    
+    public long problem2b(){
+        LinkedList<String> linkList = new LinkedList<>();
+        long start = System.currentTimeMillis(); 
+        for(int i = 0; i<1234567; i++){
+            linkList.add(Integer.toString(i));
+        }
+        long end = System.currentTimeMillis();
+        return end - start; 
+    }
 }
